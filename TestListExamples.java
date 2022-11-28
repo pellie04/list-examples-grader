55f989ee03ccbf3b361cb7748932b88970203ce8
pellie04 <pellie@ucsd.edu>
2022-11-28 10:53:46 -0800

    new

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class myChecker implements StringChecker{
+  public boolean checkString(String s){
+    return true;
+  }
+}
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void test1Filter(){
+    List<String> input1 = new ArrayList<>();
+        input1.add("cool");
+        input1.add("color");
+        input1.add("hello");
+        
+        assertArrayEquals(input1.toArray() , ListExamples.filter(input1, new myChecker()).toArray());
+  }
+  @Test
+  public void testNullFilter(){
+    List<String> input1 = new ArrayList<>();
+        
+        
+        assertArrayEquals(input1.toArray() , ListExamples.filter(input1, new myChecker()).toArray());
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,24 @@
 # Create your grading script here
 
-set -e
 
+CPATH=".:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar"
+#SCORE= 0
 rm -rf student-submission
+rm ListExamples.java
 git clone $1 student-submission
 
+cd student-submission
+cp ListExamples.java ../
+cd ..
+if [ -e ListExamples.java ]
+then
+echo "File found"
+
+else
+echo "Wrong file or file missing."
+
+fi
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestListExamples > testout.txt
+
+cat testout.txt
